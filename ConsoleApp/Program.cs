afd4fd00680bd483dffeb7504842c736d09a90fa
MarkoPasaric <mapasaric@vsite.hr>
2024-10-28 20:57:51 +0100

    Napravio 1. zadatak, 2. i 3. nekad tokom tjedna

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Proizvod proizvod = new Proizvod("Čokoladni prutić", 1.5m, 350);
+            proizvod.IspisiInformacije(proizvod);
         }
+    }
+}
+
+class Proizvod
+{
+    public string Naziv;
+    public decimal Cijena;
+    public int DostupnaKolicina;
+
+    public Proizvod(string naziv, decimal cijena, int dostupnaKolicina)
+    {
+        this.Naziv = naziv;
+        this.Cijena = cijena;
+        this.DostupnaKolicina = dostupnaKolicina;
+    }
+
+    public void IspisiInformacije()
+    {
+        Console.WriteLine($"Naziv={Naziv}, Cijena {Naziv} iznosi {Cijena} eura, a dostupna količina je {DostupnaKolicina}");
     }
 }
