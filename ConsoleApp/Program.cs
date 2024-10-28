91db011a00950a307ed96f934394d0ad85a130e1
Tomislav Prstec <toprstec@vsite.hr>
2024-10-28 20:57:38 +0100

    tprstec_lab01 Riješeno

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,10 +1,70 @@
 ﻿namespace ConsoleApp
 {
+    class Proizvod
+    {
+        public string Naziv;
+        public double Cijena;
+        public int DostupnaKolicina;
+
+        public Proizvod (string naziv, double cijena, int dostupnaKolicina)
+        {
+            Naziv = naziv;
+            Cijena = cijena;
+            DostupnaKolicina = dostupnaKolicina;
+        }
+
+        public void IspisiInformacije()
+        {
+            Console.WriteLine($"Naziv: {Naziv}");
+            Console.WriteLine($"Cijena: {Cijena} €");
+            Console.WriteLine($"Dostupna Količina: {DostupnaKolicina}");
+        }
+    }
+
+    class ElektronickiUredjaj
+    {
+        public string Naziv;
+        public string Proizvodjac;
+        public int GodinaProizvodnje;
+
+        public ElektronickiUredjaj(string naziv, string proizvodjac, int godProizvodnje)
+        {
+            Naziv = naziv;
+            Proizvodjac = proizvodjac;
+            GodinaProizvodnje = godProizvodnje;
+        }
+
+        public void IspisiInformacije()
+        {
+            Console.WriteLine($"Naziv: {Naziv}");
+            Console.WriteLine($"Proizvodjac: {Proizvodjac}");
+            Console.WriteLine($"Godina proizvodnje: {GodinaProizvodnje}");
+        }
+    }
+
+    class PametniTelefon : ElektronickiUredjaj
+    {
+        public string OperativniSustav;
+
+        public PametniTelefon(string naziv, string proizvodjac, int godProizvodnje, string opSustav) : base(naziv, proizvodjac, godProizvodnje)
+        {
+            OperativniSustav = opSustav;
+        }
+
+        public void InstalirajAplikaciju(string nazivAp)
+        {
+            Console.WriteLine($"Instaliranje aplikacije: {nazivAp}");
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Proizvod proizvod = new Proizvod("Mobitel", 1499.99, 10);
+            proizvod.IspisiInformacije();
+            PametniTelefon smartphone = new PametniTelefon("Iphone 16", "Apple", 2024, "MacOS");
+            smartphone.IspisiInformacije();
+            smartphone.InstalirajAplikaciju("Rezultati");
         }
     }
 }
