9327e381b804bba0baac75e2427d26455fd40d6e
ivan0301 <ivangrgic0301@gmail.com>
2024-10-29 20:43:46 +0100

    napravljen prvi zadatak cijeli

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,10 +1,32 @@
 ﻿namespace ConsoleApp
 {
-    internal class Program
+    internal class Proizvod
     {
-        static void Main(string[] args)
+        public string naziv;
+        public decimal cijena;
+        public int dostupnaKolicina;
+
+        public Proizvod(string naziv, decimal cijena, int dostupnaKolicina)
         {
-            Console.WriteLine("Hello, World!");
+            this.naziv = naziv;
+            this.cijena = cijena;
+            this.dostupnaKolicina = dostupnaKolicina;
+        }
+
+        public void IspisInfomracija()
+        {
+            Console.WriteLine($"Naziv je {naziv}");
+            Console.WriteLine($"Cijena je {cijena}");
+            Console.WriteLine($"Dostupna kolicina je {dostupnaKolicina}");
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Proizvod proizvod = new Proizvod("Telefon", 199.99m, 72);
+                proizvod.IspisInfomracija();
+            }
         }
     }
 }
