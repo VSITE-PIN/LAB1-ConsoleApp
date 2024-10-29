e5878c1592f479cecbabbf537cd6c4393323b212
ivan0301 <ivangrgic0301@gmail.com>
2024-10-29 20:58:28 +0100

    rjesen drugi zadatak isto

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace ConsoleApp
+﻿using System;
+
+namespace ConsoleApp
 {
     internal class Proizvod
     {
@@ -19,14 +21,61 @@
             Console.WriteLine($"Cijena je {cijena}");
             Console.WriteLine($"Dostupna kolicina je {dostupnaKolicina}");
         }
+    }
 
-        class Program
+    internal class ElektronickiUredjaj
+    {
+        public string naziv;
+        public string proizvodjac;
+        public int godinaProizvodnje;
+
+        public ElektronickiUredjaj(string naziv, string proizvodjac, int godinaProizvodnje)
         {
-            static void Main(string[] args)
-            {
-                Proizvod proizvod = new Proizvod("Telefon", 199.99m, 72);
-                proizvod.IspisInfomracija();
-            }
+            this.naziv = naziv;
+            this.proizvodjac = proizvodjac;
+            this.godinaProizvodnje = godinaProizvodnje;
+        }
+
+        public virtual void IspisiInformacije()
+        {
+            Console.WriteLine($"Naziv je {naziv}");
+            Console.WriteLine($"Proizvođač je {proizvodjac}");
+            Console.WriteLine($"Godina proizvodnje je {godinaProizvodnje}");
         }
     }
+
+    internal class PametniTelefon : ElektronickiUredjaj
+    {
+        public string operativniSustav;
+
+        public PametniTelefon(string naziv, string proizvodjac, int godinaProizvodnje, string operativniSustav)
+            : base(naziv, proizvodjac, godinaProizvodnje)
+      {
+           this.operativniSustav = operativniSustav;
+       }
+
+        public void InstalirajAplikaciju(string nazivAplikacije)
+       {
+           Console.WriteLine($"instal aplikacije pod nazivom {nazivAplikacije} na {operativniSustav}");
+      }
+
+        public override void IspisiInformacije()
+       {
+            base.IspisiInformacije();
+            Console.WriteLine($"Operativni sustav je {operativniSustav}");
+       }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Proizvod proizvod = new Proizvod("Telefon", 199.99m, 72);
+            proizvod.IspisInfomracija();
+
+            PametniTelefon telefon = new PametniTelefon("IPhone", "apple", 2024, "iOS");
+            telefon.IspisiInformacije();
+            telefon.InstalirajAplikaciju("WhatsApp");
+      }
+   }
 }
