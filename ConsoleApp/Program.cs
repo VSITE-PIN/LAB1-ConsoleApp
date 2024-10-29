a032b7c9c209a788667317c27a8fcb9dc88a3c7a
Kristijan Popović <krpopovi@vsite.hr>
2024-10-29 21:14:08 +0100

    Lab 1 zadatak 2 rj

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -30,8 +30,52 @@
             Proizvod proizvod = new Proizvod("Laptop", 1200.50m, 5);
 
             proizvod.IspisiInformacije();
+
+            PametniTelefon telefon = new PametniTelefon("Galaxy S21", "Samsung", 2021, "Android");
+
+            telefon.IspisiInformacije();
+            telefon.InstalirajAplikaciju("WhatsApp");
+        }
+    }
+
+    public class ElektronickiUredjaj
+    {
+        public string Naziv { get; set; }
+        public string Proizvod { get; set; }
+        public int GodinaProizvodnje { get; set; }
+
+        public ElektronickiUredjaj(string naziv, string proizvod, int godinaProizvodnje)
+        {
+            Naziv = naziv;
+            Proizvod = proizvod;
+            GodinaProizvodnje = godinaProizvodnje;
+        }
+        public void IspisiInformacije()
+        {
+            Console.WriteLine($"Naziv: {Naziv}");
+            Console.WriteLine($"Proizvođač: {Proizvod}");
+            Console.WriteLine($"Godina proizvodnje: {GodinaProizvodnje}");
+        }
+    }
+    public class PametniTelefon : ElektronickiUredjaj
+    {
+        public string OperativniSustav { get; set; }
+        public PametniTelefon(string naziv, string proizvod, int godinaProizvodnje, string operativniSustav)
+           : base(naziv, proizvod, godinaProizvodnje)
+        {
+            OperativniSustav = operativniSustav;
+        }
+        public new void IspisiInformacije()
+        {
+            base.IspisiInformacije();
+            Console.WriteLine($"Operativni sustav: {OperativniSustav}");
+        }
+        public void InstalirajAplikaciju(string nazivAplikacije)
+        {
+            Console.WriteLine($"Instaliranje aplikacije: {nazivAplikacije}");
         }
     }
 }
 
-    
+
+
