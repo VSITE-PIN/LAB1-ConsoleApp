ef356ff506003c181fe8100b6b555a3eb40bad61
Kristijan Popović <krpopovi@vsite.hr>
2024-10-29 20:57:11 +0100

    Labos 1 rjesanje zad 1

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,10 +1,37 @@
 ﻿namespace ConsoleApp
 {
-    internal class Program
+    public class Proizvod
+    {
+        public string Naziv { get; set; }
+        public decimal Cijena { get; set; }
+        public int DostupnaKolicina { get; set; }
+        public Proizvod(string naziv, decimal cijena, int dostupnaKolicina)
+        {
+            Naziv = naziv;
+            Cijena = cijena;
+            DostupnaKolicina = dostupnaKolicina;
+
+        }
+        public void PrikaziDetalje()
+        {
+            Console.WriteLine($"Naziv: {Naziv}, Cijena: {Cijena:C}, Dostupna Količina: {DostupnaKolicina}");
+        }
+        public void IspisiInformacije()
+        {
+            Console.WriteLine($"Naziv : {Naziv}");
+            Console.WriteLine($"Cijena: {Cijena}");
+            Console.WriteLine($"Dostupna kolicina: {DostupnaKolicina}");
+        }
+    }
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Proizvod proizvod = new Proizvod("Laptop", 1200.50m, 5);
+
+            proizvod.IspisiInformacije();
         }
     }
 }
+
+    
