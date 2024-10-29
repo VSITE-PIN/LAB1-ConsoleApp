1b5c9a6f747c9049cd7a1bb76138c3b075ed88cc
Filip Maretić <fimareti@vsite.hr>
2024-10-29 21:15:39 +0100

    LAB1 zadatak 1,2

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -23,5 +23,46 @@ class Program
     {
         Proizvod p = new Proizvod("USB", 8.59m, 50);
         p.IspisiInformacije();
+        {
+            PametniTelefon telefon = new PametniTelefon("iPhone", "Apple", 2022, "iOS");
+            telefon.IspisiInformacije();
+            telefon.InstalirajAplikaciju("WhatsApp");
+        }
+    }
+}
+
+
+public class ElektronickiUredjaj
+{
+    public string Naziv { get; set; }
+    public string Proizvodjac { get; set; }
+    public int GodinaProizvodnje { get; set; }
+
+    public ElektronickiUredjaj(string naziv, string proizvodjac, int godinaProizvodnje)
+    {
+        Naziv = naziv;
+        Proizvodjac = proizvodjac;
+        GodinaProizvodnje = godinaProizvodnje;
+    }
+
+    public void IspisiInformacije()
+    {
+        Console.WriteLine($"Naziv: {Naziv}, Proizvođač: {Proizvodjac}, Godina proizvodnje: {GodinaProizvodnje}");
+    }
+}
+
+public class PametniTelefon : ElektronickiUredjaj
+{
+    public string OperativniSustav { get; set; }
+
+    public PametniTelefon(string naziv, string proizvodjac, int godinaProizvodnje, string operativniSustav)
+        : base(naziv, proizvodjac, godinaProizvodnje)
+    {
+        OperativniSustav = operativniSustav;
+    }
+
+    public void InstalirajAplikaciju(string nazivAplikacije)
+    {
+        Console.WriteLine($"Instalirana aplikacija: {nazivAplikacije}");
     }
 }
